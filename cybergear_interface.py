2c8db915a60c0f4ddbdacdbaf8d62963909a325e
mayerhuang <144423723+mayerhuang@users.noreply.github.com>
2023-10-31 13:31:29 +0800

    Update cybergear_interface.py

--- cybergear_interface.py
+++ cybergear_interface.py
@@ -1,115 +1,149 @@
-import logging
-import serial
-import time
 import os
+import serial
 import struct
+import time
 
-result = os.popen("sudo ls -l /dev/ttyCH341*").read()
-# sudo chmod 666 /dev/ttyACM* 有时会报错，显示无曲线打开串口，这时需运行左侧命令
-com = result.split()[-1]
-os.system("sudo chmod 777 " + com)
-os.system("sudo fuser -k " + com)
-ser = serial.Serial(com, 921600, timeout=1)
+# 初始化串口
 
-print(f"进入<AT命令模式>")
-ser.write(bytes.fromhex('41 54 2b 41 54 0d 0a'))
 
-#参数转通信
+def initialize_serial_port():
+    result = os.popen("sudo ls -l /dev/ttyCH341*").read()
+    com = result.split()[-1]
+    os.system(f"sudo chmod 777 {com}")
+    os.system(f"sudo fuser -k {com}")
+    ser = serial.Serial(com, 921600, timeout=1)
+    ser.write(bytes.fromhex('41 54 2b 41 54 0d 0a'))
+    return ser
+
+# 将浮点数转换为字节表示的字符串
+
+
 def float_to_bytes(f):
-    """将浮点数转换为带空格的字节表示字符串"""
     byte_val = struct.pack('<f', f)
     return ' '.join('{:02x}'.format(b) for b in byte_val)
 
-#通信转参数
+# 将字节转换为对应的命令文本
+
+
+def bytes_to_text(cmd_x):
+    cmd_dict = {
+        '00': ' 获取设备 ID', '01': '运控模式电机控制指令', '02': '电机反馈数据',
+        # ... 其他命令映射 ...
+        '90': '设置参数'
+    }
+    return cmd_dict.get(cmd_x, cmd_x)  # 返回对应的命令或默认为字节
+
+# 将带空格的字节表示字符串转换为浮点数
 
 
 def bytes_to_float(byte_str):
-    """将带空格的字节表示字符串转换为浮点数"""
-    bytes_without_space = bytes.fromhex(byte_str.replace(' ', ''))
-    return struct.unpack('<f', bytes_without_space)[0]
+    bytes_val = bytes.fromhex(byte_str.replace(' ', ''))
+    float_val = struct.unpack('<f', bytes_val)[0]
+    return round(float_val, 4)
 
-#通信转CANID
+# 根据输入值转换为对应的字节
 
 
 def bytes_to_value(value):
-    # Convert integer to hex string
-    hex_str = "{:04x}".format(value)
-    # Extract the high byte from the hex string
+    hex_str = "{:04x}".format(value) if not isinstance(value, str) else value
     high_byte = int(hex_str[:2], 16)
-    # Return the calculated value
     return 123 + (high_byte - 0xDC) // 8
 
+# 将CANID转换为字节
 
-#CANID转通信
+
 def value_to_bytes(value):
-    # 根据规律计算高位字节
     high_byte = 0xDC + (value - 123) * 8
-    # 返回字节字符串
     return '{:02x} 08'.format(high_byte)
 
-
-def parse_command(command_str):
-    return [int(x, 16) for x in command_str.split()]
+# 提取命令信息
 
 
-def reverse_bytes(hex_str):
-    # 去除前缀"0X"或"0x"
-    hex_str = hex_str[2:] if hex_str.startswith(('0X', '0x')) else hex_str
-    # 确保输入长度为偶数，为每个字节提供两个字符
-    if len(hex_str) % 2 != 0:
-        hex_str = '0' + hex_str
+def extract_info(parsed_command):
+    return parsed_command[4:6], parsed_command[10:14], parsed_command[14:18], parsed_command[22:30]
 
-    # 使用列表推导反转字节顺序，并使用join方法将其组合成字符串
-    reversed_str = ' '.join([hex_str[i:i+2]
-                            for i in range(0, len(hex_str), 2)][::-1])
-    return reversed_str
+# 整数转为反向的十六进制
 
 
-def send_command(can_id, cmd_mode, index, value):
-    id_num = value_to_bytes(can_id)
-    index_num = reverse_bytes(hex(index))
+def int_to_reversed_hex(value):
+    hex_str = "{:04x}".format(value)
+    return ' '.join([hex_str[i:i+2].upper() for i in range(2, -1, -2)])
+
+# 反转十六进制字节
+
+
+def reverse_hex_bytes(hex_str):
+    return "0x" + hex_str[2:] + hex_str[:2]
+
+# 发送命令到串口
+
+
+def send_command(ser, can_id, cmd_mode, index, value):
+    id_num, index_num = value_to_bytes(can_id), int_to_reversed_hex(index)
     value_num = float_to_bytes(value)
     date = f"41 54 {cmd_mode} 07 eb {id_num} {index_num} 00 00 {value_num} 0d 0a"
+    print(f"发送L91指令：{cmd_mode}至CAN{can_id}:索引{index},值{value}")
+
     try:
-        # 将电机向前旋转1秒
         ser.write(bytes.fromhex(date))
         response = ser.readline()
         hex_string = ' '.join([response[i:i+1].hex()
-                               for i in range(len(response))])
-        print(f"RX: {hex_string}")
-        time.sleep(1)
-
+                              for i in range(len(response))])
+        cmd_x, can_id_x, index_x, value_x = extract_info(
+            hex_string.replace(" ", ""))
+        cmd_int, can_id_int = bytes_to_text(cmd_x), bytes_to_value(can_id_x)
+        index_int, value_int = reverse_hex_bytes(
+            index_x), bytes_to_float(value_x)
+        print(
+            f"接收内容《类型: {cmd_int} canid: {can_id_int} index: {index_int} value: {value_int}》RX: {hex_string}")
     except Exception as e:
         print(f"An error occurred: {e}")
+
     return hex_string
 
-#测试代码
-print('这个不知道啥意思，有使能的作用')
-ser.write(bytes.fromhex('41 54 90 07 eb fc 08 05 70 00 00 01 00 00 00 0d 0a'))
-response = ser.readline()
-print(f"RX: {response}")
-time.sleep(5)
-print('模式1位置模式')
-send_command(127, '18', 0X7005, 1)
-print('电流限制')
-send_command(127, '90', 0X7018, 4)
-print('速度限制')
-send_command(127, '90', 0X7017, 1)
-print('角度限制')
-send_command(127, '90', 0X7016, 10)
-time.sleep(1)
-print('角度限制')
-send_command(127, '90', 0X7016, 5)
-time.sleep(1)
-print('角度限制')
-send_command(127, '90', 0X7016, 10)
-time.sleep(1)
-print('角度限制')
-send_command(127, '90', 0X7016, 5)
-time.sleep(1)
-print('角度限制')
-send_command(127, '90', 0X7016, 10)
-time.sleep(5)
-print('停止')
-ser.write(bytes.fromhex('41 54 90 07 eb fc 08 05 70 00 00 07 00 7f ff 0d 0a'))
-#ser.close()
+# 主函数测试
+
+
+def main():
+    ser = initialize_serial_port()
+    print('位置模式')
+    ser.write(bytes.fromhex('41 54 90 07 eb fc 08 05 70 00 00 01 00 00 00 0d 0a'))
+    response = ser.readline()
+    hex_string = ' '.join([response[i:i+1].hex()
+                          for i in range(len(response))])
+    cmd_x, can_id_x, index_x, value_x = extract_info(
+        hex_string.replace(" ", ""))
+    cmd_int, can_id_int = bytes_to_text(cmd_x), bytes_to_value(can_id_x)
+    index_int, value_int = reverse_hex_bytes(index_x), bytes_to_float(value_x)
+    print(
+        f"接收内容《类型: {cmd_int} canid: {can_id_int} index: {index_int} value: {value_int}》RX: {hex_string}")
+    print('设置模式1')
+    send_command(ser,127, '18', 28677, 1)
+    print('电流限制')
+    send_command(ser, 127, '90', 28696, 4)
+    print('速度限制')
+    send_command(ser, 127, '90', 28695, 1)
+    print('角度限制')
+    #π=180度
+    send_command(ser, 127, '90', 28694, 10)
+    time.sleep(1)
+    print('角度限制')
+    send_command(ser, 127, '90', 28694, 5)
+    time.sleep(1)
+    print('角度限制')
+    send_command(ser, 127, '90', 28694, 10)
+    time.sleep(1)
+    print('角度限制')
+    send_command(ser, 127, '90', 28694, 5)
+    time.sleep(1)
+    print('角度限制')
+    send_command(ser, 127, '90', 28694, 0)
+    time.sleep(5)
+    print('重置模式')
+    #ser.write(bytes.fromhex('41 54 90 07 eb fc 08 05 70 00 00 07 00 7f ff 0d 0a'))#停止
+    ser.write(bytes.fromhex('41 54 20 07 eb fc 08 00 00 00 00 00 00 00 00 0d 0a'))
+    # ser.close()
+
+
+if __name__ == "__main__":
+    main()
